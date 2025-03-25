01b04698ab183113a1f5e6872e51cca2c73dffbd
Sebastian-Rothe <sebi1995@gmx.at>
2025-03-25 20:26:12 +0100

    basic auth functions added

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,41 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  constructor() { }
+  user$: Observable<any>;
 
+  constructor(private auth: AngularFireAuth) {
+    this.user$ = this.auth.authState;
+  }
+
+  async login(email: string, password: string) {
+    try {
+      return await this.auth.signInWithEmailAndPassword(email, password);
+    } catch (error) {
+      console.error('Error logging in:', error);
+      throw error;
+    }
+  }
+
+  async register(email: string, password: string) {
+    try {
+      return await this.auth.createUserWithEmailAndPassword(email, password);
+    } catch (error) {
+      console.error('Error registering:', error);
+      throw error;
+    }
+  }
+
+  async logout() {
+    try {
+      await this.auth.signOut();
+    } catch (error) {
+      console.error('Error logging out:', error);
+      throw error;
+    }
+  }
 }
