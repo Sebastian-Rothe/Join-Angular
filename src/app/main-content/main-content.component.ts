1a307c704d3c6aa7b54106811020c2cbb300b720
Sebastian-Rothe <sebi1995@gmx.at>
2025-03-28 17:25:18 +0100

    hader and nav to main-content - and no padding at app-c.

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../components/header/header.component';
+import { NavbarComponent } from '../components/navbar/navbar.component';
 
 @Component({
   selector: 'app-main-content',
-  imports: [],
+  imports: [HeaderComponent, NavbarComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
