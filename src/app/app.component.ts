a0dcac9b4abbd726c9cad4bd9a13dcf64dd448b7
Sebastian-Rothe <sebi1995@gmx.at>
2025-03-31 17:12:59 +0200

    added a 'please rotate your device!' screen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
 import { HeaderComponent } from './components/header/header.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'],
+  standalone: true,
+  imports: [RouterOutlet, MatIconModule]
 })
 export class AppComponent {
   title = 'Join-Angular';
