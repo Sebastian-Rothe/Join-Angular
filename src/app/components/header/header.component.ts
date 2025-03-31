453aeab3c4e8fa9a3b8499d8475dfdb71ba57374
Sebastian-Rothe <sebi1995@gmx.at>
2025-03-31 15:44:02 +0200

    mat-icon for header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
+  imports: [MatIconModule],
+  standalone: true
 })
 export class HeaderComponent {
   isDropdownOpen = false;
