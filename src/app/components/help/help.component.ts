f78ab44e91a33f7dcebd3bf8dff39d77c444d82b
Sebastian-Rothe <sebi1995@gmx.at>
2025-04-22 14:54:58 +0200

    mat icon for help page

--- src/app/components/help/help.component.ts
+++ src/app/components/help/help.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { Location } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-help',
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './help.component.html',
   styleUrl: './help.component.scss'
 })
 export class HelpComponent {
+  constructor(private location: Location) {}
 
+  goBack() {
+    this.location.back();
+  }
 }
