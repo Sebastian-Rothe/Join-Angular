bac41e48f81f305d29a66f57c18f941e44b5f23f
Sebastian-Rothe <sebi1995@gmx.at>
2025-05-02 11:17:35 +0200

    first step summary

--- src/app/components/summary/summary.component.ts
+++ src/app/components/summary/summary.component.ts
@@ -1,11 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-summary',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, MatIconModule],
   templateUrl: './summary.component.html',
-  styleUrl: './summary.component.scss'
+  styleUrls: ['./summary.component.scss']
 })
-export class SummaryComponent {
+export class SummaryComponent implements OnInit {
+  metrics = {
+    todo: 0,
+    done: 0,
+    urgent: 0,
+    upcomingDeadline: new Date(),
+    tasksInBoard: 0,
+    tasksInProgress: 0,
+    awaitingFeedback: 0
+  };
 
+  greeting = '';
+  userName = '';
+
+  ngOnInit() {
+    this.loadMetrics();
+    this.setGreeting();
+  }
+
+  private loadMetrics() {
+    // TODO: Implement loading metrics from service
+  }
+
+  private setGreeting() {
+    const hour = new Date().getHours();
+    if (hour < 12) this.greeting = 'Good morning';
+    else if (hour < 17) this.greeting = 'Good afternoon';
+    else this.greeting = 'Good evening';
+    
+    // TODO: Get actual user name
+    this.userName = 'Guest';
+  }
 }
