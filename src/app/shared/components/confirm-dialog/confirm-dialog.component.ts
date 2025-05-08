20c659b1fddb64ae587e9b724ead5859a0bdc4c7
Sebastian-Rothe <sebi1995@gmx.at>
2025-05-08 15:30:08 +0200

    design for confirm msg adjusted

--- src/app/shared/components/confirm-dialog/confirm-dialog.component.ts
+++ src/app/shared/components/confirm-dialog/confirm-dialog.component.ts
@@ -1,8 +1,14 @@
 import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-confirm-dialog',
+  standalone: true,
+  imports: [
+    MatIconModule
+  
+  ],
   templateUrl: './confirm-dialog.component.html',
   styleUrls: ['./confirm-dialog.component.scss'],
 })
